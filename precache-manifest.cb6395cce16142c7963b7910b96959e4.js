f122850aa8d2c8ee0af86159c520a95e08a30704
Ankit Upadhyay <56730118+ankitupadhyay889@users.noreply.github.com>
2020-09-13 17:15:03 +0530

    Updates

---
+++ precache-manifest.cb6395cce16142c7963b7910b96959e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f5426c3343709e67a0c2cce3d32f8c",
+    "url": "/Haramkhoreresturent/index.html"
+  },
+  {
+    "revision": "c2cc5e5f3d931f20cb44",
+    "url": "/Haramkhoreresturent/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1e5bd1f1a1b6a9eef0a5",
+    "url": "/Haramkhoreresturent/static/css/main.55914105.chunk.css"
+  },
+  {
+    "revision": "c2cc5e5f3d931f20cb44",
+    "url": "/Haramkhoreresturent/static/js/2.61db185d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Haramkhoreresturent/static/js/2.61db185d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5bd1f1a1b6a9eef0a5",
+    "url": "/Haramkhoreresturent/static/js/main.6519f703.chunk.js"
+  },
+  {
+    "revision": "05b7f83a4650ed3e20fb",
+    "url": "/Haramkhoreresturent/static/js/runtime-main.22cf3c75.js"
+  },
+  {
+    "revision": "cb40b70fb80865d3655b2e4e3f3b9ed9",
+    "url": "/Haramkhoreresturent/static/media/img3.cb40b70f.jpg"
+  }
+]);
